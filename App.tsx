cbfe2aeaf55222c7333523a59f731c4507fe23fd
Rodrigo Teixeira <r.teixeira1@outlook.com.br>
2024-12-01 08:12:21 -0300

    feat:Create signup screen

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import { GluestackUIProvider, Text, Center } from "@gluestack-ui/themed";
 import { config } from "./config/gluestack-ui.config";
 import { Loading } from "@components/Loading";
 import  { Signin } from "@screens/Signin"
+import { SignUp } from "@screens/SignUp";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -6,7 +6,7 @@ import Logo from "@assets/logo.svg";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
-export function Signin() {
+export function SignUp() {
   return (
     <VStack flex={1} bg="$gray700">
       <Image
@@ -20,41 +20,48 @@ export function Signin() {
 
       <VStack flex={1} px="$10" pb="$16">
         <Center my="$24">
-          <Logo />
+          <Logo/>
 
-          <Text color="$#7C7C8A" fontSize="$3xl" mt="$2" >
+          <Text color="$#f2f2f9" fontSize="$3xl" mt="$1">
             Espaço estetica
           </Text>
         </Center>
 
-        <Center gap="$2">
-          <Heading color="$gray100">Acesse a conta</Heading>
+        <Center gap="$2" flex={1}>
+          <Heading color="$gray100">Crie sua conta</Heading>
+
+          <Input placeholder="Nome" style={{ color: "#000000" }} />
 
           <Input
             placeholder="E-mail"
             keyboardType="email-address"
             autoCapitalize="none"
-            style= {{color: "#000000" }}
-          />
-          <Input 
-            placeholder="senha" 
-            secureTextEntry 
-            style={{ color: "#000000"}}
+            style={{ color: "#000000" }}
           />
 
-          <Button 
-            title="Acessar" 
-            />
-        
+          <Input
+            placeholder="senha"
+            secureTextEntry
+            style={{ color: "#000000" }}
+          />
+
+          <Input
+            placeholder="Confirme a senha"
+            secureTextEntry
+            style={{ color: "#000000" }}
+          />
+
+          <Button title="Criar e acessar" />
         </Center>
 
-        <Center flex={1} justifyContent="flex-end" mt="$4">
-          <Text color="$#FFF" fontSize="$sm" mb="$3" fontFamily="$body">
+        <Center justifyContent="flex-end" mt="$6">
+          <Text color="$#FFFFFF" fontSize="$sm" mb="$6" fontFamily="$body">
             Ainda não tem acesso?
           </Text>
 
-          <Button title="Criar Conta" variant="outline"/>
+          <Button title="Voltar para login" variant="outline" mt="$2" />
         </Center>
+
       </VStack>
     </VStack>
   );
